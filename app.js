c548b840208314d859ad597ba47b9c7b821c9734
Carri-anne Hamer <chamer79@gmail.com>
2021-04-19 15:44:46 -0400

    requesting API - ingredients. Error

--- app.js
+++ app.js
@@ -1,39 +1,57 @@
     // -- Create Dynamic Dropdown Menu --
-function baseSpirit() {
+function menuOptions() {
   const selectTag = document.querySelector("#select-spirit")
   const spirit = ["Bourbon", "Gin", "Tequila", "Vodka", "Rum"]
   
   for (let i = 0; i < spirit.length; i++) {
     // console.log(spirit[i])   // <-- sanity check
     let spiritList = spirit[i]
-    let optionTag = document.createElement("option")
+    let optionTag = document.createElement("option")  // <-- Create For Option Tags
 
-    optionTag.textContent = spirit[i]
+    optionTag.textContent = spirit[i]   
     optionTag.value = spirit
     selectTag.appendChild(optionTag)
   }
 }
-baseSpirit()
+menuOptions()
     
     
     // -- Create For Option Tags --
-
+  // * Refer to lines 9-13
 
     
     
     // -- Get Option Value Tag --
-
+function getValue(e) {
+  e.preventDefault()
+  const optionValue = document.querySelector("#select-spirits")
+  console.log(optionValue)  // <--sanity check  coming up as null at line 44
+  // getDrinkImage(optionValue)
+  return optionValue
+}
     
     
     
     // -- Event Handler for Form --
-
+const form = document.querySelector("form")
+form.addEventListener("submit", getValue)
     
     
     
     // -- API Request for Ingredients Data --
+async function baseSpirit(spirit) {
+  menuOptions = spirit
+  const url = `www.thecocktaildb.com/api/json/v1/1/filter.php?i=${spirit}`
 
-
+  try {
+    const response = await axios.get(url)
+    console.log(response) //<--sanity check
+  }
+  catch (error) {
+    console.error (error)
+  }
+}
+baseSpirit()
 
     
     // -- API Request for Random Coctail Recipe Data --
