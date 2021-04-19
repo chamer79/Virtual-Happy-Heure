dc88f466f8c36fde818954d315603cbef4da364e
Carri-anne Hamer <chamer79@gmail.com>
2021-04-19 19:59:56 -0400

    debugging event handler

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-    // -- Create Dynamic Dropdown Menu --
-function menuOptions() {
+    // -- Create Dynamic Dropdown Menu --  <-- VERY HAPPY Possible Code Snippet
+function menuOptions(list) {
   const selectTag = document.querySelector("#select-spirit")
   const spirit = ["Bourbon", "Gin", "Tequila", "Vodka", "Rum"]
   
@@ -12,6 +12,7 @@ function menuOptions() {
     optionTag.value = spirit
     selectTag.appendChild(optionTag)
   }
+  return list
 }
 menuOptions()
     
@@ -25,8 +26,8 @@ menuOptions()
 function getValue(e) {
   e.preventDefault()
   const optionValue = document.querySelector("#select-spirits")
-  console.log(optionValue)  // <--sanity check  coming up as null at line 44
-  
+  console.log(optionValue)  // <--sanity check  
+  // getDrinkId(optionValue)
   return optionValue
 }
     
@@ -39,23 +40,27 @@ form.addEventListener("submit", getValue)
     
     
     // -- API Request for Ingredients Data --
-async function baseSpirit(spirit) {
-  menuOptions = spirit
-  const url = (`www.thecocktaildb.com/api/json/v1/1/filter.php?i=gin`)
+async function getDrinkId(spiritValue) {
+  // menuOptions = spiritValue
+  const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${spiritValue}`  // <-- NEEDED to input 'https://' in order to request the API
 
   try {
-    const response = await axios.get(url)
-    console.log(response) //<--sanity check
+    const spiritResponse = await axios.get(url)
+    console.log(spiritResponse.data.idDrink) //<--sanity check  logging 'undefinded'
+    const spiritDrinkId = spiritResponse.data.idDrink
+ 
+    // menuOptions(spiritList)  // <--RefenceError: spiritList is not defined @ getDrinkId
   }
   catch (error) {
     console.error (error)
   }
+  return spiritValue
 }
-baseSpirit()
+getDrinkId()
 
     
     // -- API Request for Random Coctail Recipe Data --
-
+// async function 
 
 
     
@@ -73,4 +78,3 @@ baseSpirit()
 
 
 
-    
