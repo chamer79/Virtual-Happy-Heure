cf47c8a6d4b36499a9ffaf19f6b508a74b4e537e
Carri-anne Hamer <chamer79@gmail.com>
2021-04-19 10:47:03 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+    // -- Create Dynamic Dropdown Menu --
+
+
+    
+    
+    // -- Create For Option Tags --
+
+
+    
+    
+    // -- Get Option Value Tag --
+
+    
+    
+    
+    // -- Event Handler for Form --
+
+    
+    
+    
+    // -- API Request for Ingredients Data --
+
+
+
+    
+    // -- API Request for Random Coctail Recipe Data --
+
+
+
+    
+    // --Connecting both APIs together --
+
+
+
+    
+    // --Create Dynamic Tags & Append to the DOM: Image & Article
+
+
+
+
+    // -- Remove Previous Results
+
+
+
+    
